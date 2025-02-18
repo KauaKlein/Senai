52b22bb06aa551be7d8f3ab01d019eb973a84fdf
klein <kaua1floripa@gmail.com>
2025-02-18 16:14:45 -0300

    branchJogo

--- script.js
+++ script.js
@@ -1,2 +1,15 @@
 const canvas = document.getElementById("joguin");
 const ctx = canvas.getContext('2d');
+
+function desenhaPersonagem(){
+    ctx.fillStyle = "yellow"
+    ctx.fillRect(100, canvas.height -120,50,50)
+}
+
+function loop (){
+
+    desenhaPersonagem()
+    requestAnimationFrame(loop)
+}
+
+loop()
